bffedfdc19ea508cd575ebfd81e6703104751e2d
hoangchitrung <trunghoang1124@gmail.com>
2025-12-09 14:21:29 +0700

    [add] lab-04 hash

---
+++ lab-04/hash/blake2.py
@@ -0,0 +1,20 @@
+import hashlib
+
+
+def blake2(message):
+    # digest_size=64 tương đương 512 bit (Blake2b)
+    blake2_hash = hashlib.blake2b(digest_size=64)
+    blake2_hash.update(message)
+    return blake2_hash.digest()
+
+
+def main():
+    text = input("Nhập chuỗi văn bản: ").encode("utf-8")
+    hashed_text = blake2(text)
+
+    print("Chuỗi văn bản đã nhập:", text.decode("utf-8"))
+    print("BLAKE2 Hash:", hashed_text.hex())
+
+
+if __name__ == "__main__":
+    main()
