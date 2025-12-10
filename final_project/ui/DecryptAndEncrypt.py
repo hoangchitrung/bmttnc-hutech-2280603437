669ed8bdd6e4c0d1cf7dce1596f3568efd3b0dd8
hoangchitrung <trunghoang1124@gmail.com>
2025-12-10 20:47:21 +0700

    update UI

--- final_project/ui/DecryptAndEncrypt.py
+++ final_project/ui/DecryptAndEncrypt.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'DecryptAndEncrypt.ui'
+# Form implementation generated from reading ui file './DecryptAndEncrypt.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.11
 #
